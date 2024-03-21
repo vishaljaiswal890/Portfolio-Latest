255befd796a2b9d484a9a9da7d2b53ddc8744d7e
Vishal <92736996+vishaljaiswal890@users.noreply.github.com>
2024-03-22 00:59:22 +0530

    Deployment

--- server.js
+++ server.js
@@ -65,13 +65,17 @@ router.post("/contact", (req, res) => {
 // For local
 // app.use('/static', express.static(path.join(__dirname, './Frontend/build/static')));
 // For vercel
-app.use('/static', express.static(path.join(__dirname, './Frontend/build/static')));
-app.get('/test', (req, res) => {
-  res.send('Server is working');
-})
+// app.use('/static', express.static(path.join(__dirname, './Frontend/build/static')));
+// app.get('/test', (req, res) => {
+//   res.send('Server is working');
+// })
 // app.get('/*', function(req, res) {
 //   res.sendFile('index.html', {root: path.join(__dirname, './Frontend/build/')});
 // });
+
+app.get("/" , (req, res) => {
+  res.send("Server is working")
+})
 
 // Start server
 app.listen(PORT, () => {
